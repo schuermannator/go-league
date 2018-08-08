5dd31176da3ad07af1adebc4f06e82b12d4d985c
Zach Schuermann <zachary.zvs@gmail.com>
2018-08-07 21:17:58 -0400

    gofmt

--- scraper.go
+++ scraper.go
@@ -1,18 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-  "encoding/json"
-  "io/ioutil"
-  "time"
+	"time"
 )
 
 func get_account_id(summoner string) (int, error) {
-  api_key := url.QueryEscape("RGAPI-3c40b941-1cbe-4bc6-8b0a-c2bb3d69d118")
-  endpt := fmt.Sprintf("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/%s?api_key=%s", summoner, api_key)
+	api_key := url.QueryEscape("RGAPI-3c40b941-1cbe-4bc6-8b0a-c2bb3d69d118")
+	endpt := fmt.Sprintf("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/%s?api_key=%s", summoner, api_key)
 
 	// Build the request
 	req, err := http.NewRequest("GET", endpt, nil)
@@ -27,7 +27,7 @@ func get_account_id(summoner string) (int, error) {
 	// A Client is an HTTP client
 	client := &http.Client{}
 
-  // Send the request via a client
+	// Send the request via a client
 	// Do sends an HTTP request and
 	// returns an HTTP response
 	resp, err := client.Do(req)
@@ -43,26 +43,26 @@ func get_account_id(summoner string) (int, error) {
 
 	// Fill the record with the data from the JSON
 	//var record Numverify
-  //var data string
+	//var data string
 
 	// Use json.Decode for reading streams of JSON data
 	// if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
 	// 	log.Println(err)
 	// }
-  body, err := ioutil.ReadAll(resp.Body)
-  var data map[string]interface{}
-  if err := json.Unmarshal(body, &data); err != nil {
-      panic(err)
-  }
+	body, err := ioutil.ReadAll(resp.Body)
+	var data map[string]interface{}
+	if err := json.Unmarshal(body, &data); err != nil {
+		panic(err)
+	}
 
-  log.Print(data["accountId"])
+	log.Print(data["accountId"])
 
-  return int(data["accountId"].(float64)), nil
+	return int(data["accountId"].(float64)), nil
 }
 
 func get_matches(id int) ([]int, error) {
-  api_key := url.QueryEscape("RGAPI-3c40b941-1cbe-4bc6-8b0a-c2bb3d69d118")
-  endpt := fmt.Sprintf("https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/%d?api_key=%s", id, api_key)
+	api_key := url.QueryEscape("RGAPI-3c40b941-1cbe-4bc6-8b0a-c2bb3d69d118")
+	endpt := fmt.Sprintf("https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/%d?api_key=%s", id, api_key)
 	// Build the request
 	req, err := http.NewRequest("GET", endpt, nil)
 	if err != nil {
@@ -76,7 +76,7 @@ func get_matches(id int) ([]int, error) {
 	// A Client is an HTTP client
 	client := &http.Client{}
 
-  // Send the request via a client
+	// Send the request via a client
 	// Do sends an HTTP request and
 	// returns an HTTP response
 	resp, err := client.Do(req)
@@ -89,23 +89,23 @@ func get_matches(id int) ([]int, error) {
 	// when done reading from it
 	// Defer the closing of the body
 	defer resp.Body.Close()
-  body, err := ioutil.ReadAll(resp.Body)
-  var data map[string]interface{}
-  if err := json.Unmarshal(body, &data); err != nil {
-      panic(err)
-  }
-  var list = make([]int, 0)
-  match_list := data["matches"].([]interface{})
-  for _, match := range match_list {
-    match_map := match.(map[string]interface{})
-    list = append(list, int(match_map["gameId"].(float64)))
-  }
-  return list, nil
+	body, err := ioutil.ReadAll(resp.Body)
+	var data map[string]interface{}
+	if err := json.Unmarshal(body, &data); err != nil {
+		panic(err)
+	}
+	var list = make([]int, 0)
+	match_list := data["matches"].([]interface{})
+	for _, match := range match_list {
+		match_map := match.(map[string]interface{})
+		list = append(list, int(match_map["gameId"].(float64)))
+	}
+	return list, nil
 }
 
 func get_match_times(match_id int) (time.Time, float64, error) {
-  api_key := url.QueryEscape("RGAPI-3c40b941-1cbe-4bc6-8b0a-c2bb3d69d118")
-  endpt := fmt.Sprintf("https://na1.api.riotgames.com/lol/match/v3/matches/%d?api_key=%s", match_id, api_key)
+	api_key := url.QueryEscape("RGAPI-3c40b941-1cbe-4bc6-8b0a-c2bb3d69d118")
+	endpt := fmt.Sprintf("https://na1.api.riotgames.com/lol/match/v3/matches/%d?api_key=%s", match_id, api_key)
 	// Build the request
 	req, err := http.NewRequest("GET", endpt, nil)
 	if err != nil {
@@ -119,7 +119,7 @@ func get_match_times(match_id int) (time.Time, float64, error) {
 	// A Client is an HTTP client
 	client := &http.Client{}
 
-  // Send the request via a client
+	// Send the request via a client
 	// Do sends an HTTP request and
 	// returns an HTTP response
 	resp, err := client.Do(req)
@@ -132,48 +132,48 @@ func get_match_times(match_id int) (time.Time, float64, error) {
 	// when done reading from it
 	// Defer the closing of the body
 	defer resp.Body.Close()
-  body, err := ioutil.ReadAll(resp.Body)
-  var data map[string]interface{}
-  if err := json.Unmarshal(body, &data); err != nil {
-      panic(err)
-  }
+	body, err := ioutil.ReadAll(resp.Body)
+	var data map[string]interface{}
+	if err := json.Unmarshal(body, &data); err != nil {
+		panic(err)
+	}
 
-  if data["gameCreation"] == nil {
-    panic(data)
-  }
+	if data["gameCreation"] == nil {
+		panic(data)
+	}
 
-  creation := time.Unix(int64(data["gameCreation"].(float64)/1000), 0)
-  duration := data["gameDuration"].(float64) / 3600.
-  rounded := time.Date(creation.Year(), creation.Month(), creation.Day(), 0, 0, 0, 0, creation.Location())
-  return rounded, duration, nil
+	creation := time.Unix(int64(data["gameCreation"].(float64)/1000), 0)
+	duration := data["gameDuration"].(float64) / 3600.
+	rounded := time.Date(creation.Year(), creation.Month(), creation.Day(), 0, 0, 0, 0, creation.Location())
+	return rounded, duration, nil
 }
 
 func scrape(name string) (map[time.Time]float64, error) {
 	id, err := get_account_id(name)
 	if err != nil {
-    return nil, err
-  }
+		return nil, err
+	}
 
-  var match_list []int
-  match_list, err = get_matches(id)
+	var match_list []int
+	match_list, err = get_matches(id)
 
-  length_map := make(map[time.Time]float64)
-  for i, match := range match_list {
-    if i > 20 {
-      break
-    }
-    create, dur, er := get_match_times(match)
-    if val, ok := length_map[create]; ok {
-      // already in map
-      length_map[create] = val + dur
-    } else {
-      // not in map
-      length_map[create] = dur
-    }
-    if er != nil {
-      return nil, err
-    }
-    log.Print(create, dur)
-  }
-  return length_map, nil
+	length_map := make(map[time.Time]float64)
+	for i, match := range match_list {
+		if i > 20 {
+			break
+		}
+		create, dur, er := get_match_times(match)
+		if val, ok := length_map[create]; ok {
+			// already in map
+			length_map[create] = val + dur
+		} else {
+			// not in map
+			length_map[create] = dur
+		}
+		if er != nil {
+			return nil, err
+		}
+		log.Print(create, dur)
+	}
+	return length_map, nil
 }
--- server.go
+++ server.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"github.com/gin-gonic/gin"
 	"log"
-  "github.com/gin-gonic/gin"
-  "net/http"
-  "time"
-  "sort"
+	"net/http"
+	"sort"
+	"time"
 )
 
 // Define us a type so we can sort it
@@ -14,49 +14,52 @@ type TimeSlice []time.Time
 
 // Forward request for length
 func (p TimeSlice) Len() int {
-    return len(p) }
+	return len(p)
+}
 
 // Define compare
 func (p TimeSlice) Less(i, j int) bool {
-    return p[i].Before(p[j]) }
+	return p[i].Before(p[j])
+}
 
 // Define swap over an array
 func (p TimeSlice) Swap(i, j int) {
-    p[i], p[j] = p[j], p[i] }
+	p[i], p[j] = p[j], p[i]
+}
 
-func main()  {
-  log.Print("Starting server...")
-  router := gin.Default()
-  router.Static("/static", "./static")
-  router.LoadHTMLGlob("templates/*")
+func main() {
+	log.Print("Starting server...")
+	router := gin.Default()
+	router.Static("/static", "./static")
+	router.LoadHTMLGlob("templates/*")
 	router.GET("/", func(c *gin.Context) {
-    c.HTML(http.StatusOK, "index.html", gin.H{
+		c.HTML(http.StatusOK, "index.html", gin.H{
 			"title": "Main website",
 		})
 	})
-  router.POST("/", func(c *gin.Context) {
-    name := c.PostForm("text")
-    length_map, err := scrape(name)
-    if err != nil {
-      return
-    }
-    // Convert map to slice of keys.
-    labels := []time.Time{}
-    values := []float64{}
-    for key, _ := range length_map {
-        labels = append(labels, key)
-    }
-    sort.Sort(TimeSlice(labels))
-    for _, label := range labels {
-        values = append(values, length_map[label])
-    }
-    fmt.Println("Done scraping")
-    c.HTML(http.StatusOK, "chart.html", gin.H{
-			"title": "Chart time!",
-      "values": values,
-      "labels": labels,
-      "max": 10,
+	router.POST("/", func(c *gin.Context) {
+		name := c.PostForm("text")
+		length_map, err := scrape(name)
+		if err != nil {
+			return
+		}
+		// Convert map to slice of keys.
+		labels := []time.Time{}
+		values := []float64{}
+		for key, _ := range length_map {
+			labels = append(labels, key)
+		}
+		sort.Sort(TimeSlice(labels))
+		for _, label := range labels {
+			values = append(values, length_map[label])
+		}
+		fmt.Println("Done scraping")
+		c.HTML(http.StatusOK, "chart.html", gin.H{
+			"title":  "Chart time!",
+			"values": values,
+			"labels": labels,
+			"max":    10,
 		})
-  })
+	})
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
